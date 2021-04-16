de3910f57636677f03c0fb9cd9d491c9a30dd8fc
savio591 <savio591@hotmail.com>
2021-04-16 11:58:03 -0300

    Pagination system fixes

--- src/components/Pages.tsx
+++ src/components/Pages.tsx
@@ -8,40 +8,63 @@ export function Pages() {
   const { pages, queries } = useContext(ApiContext);
   const { searchQuery, pageQuery } = queries;
 
-  // Receber o total:
-  // Mapear o total em decimais 21 = 3
-  // Renderizar os números da paginação.
-  // Marcar a página selecionada como mark, o número selecionado estará
-
+  // Converts the total number of characters in pagination
   const pagination = parseInt((pages + 1 * 10) / 10 + 1);
-  console.log(pagination);
-
+  console.log({ pagination });
+  // pagination mapping for the buttons
   const pagingMap = [];
-  let pagingOffset = 3;
-
-  if (pageQuery > 3) {
-    pagingOffset = pageQuery;
-  }
   for (let i = 1; i < pagination; i++) {
     pagingMap.push(i);
   }
+  console.log({ pagingMap: pagingMap.length });
+
+  // Default pagination is 1 if don't have a page query.
+  // When in the query isn't a page query, will return a NaN
+  // instead a number, so the const page will parse this rule:
+  const page = isNaN(pageQuery) ? 1 : pageQuery;
+  console.log({ page });
+  // Page offset
+  let pagingOffset = 3;
+  if (pageQuery > 3) {
+    pagingOffset = pageQuery;
+    console.log({ pagingOffset });
+  }
+
+  /* Query generator */
   const query = (p: number) => {
     const searchQueryForParam =
       searchQuery === null || "" ? "" : "findChar=" + searchQuery;
     const pageQueryForParam = isNaN(pageQuery || p) ? "" : "page=" + p;
-
-    return "/?" + searchQueryForParam + "&" + pageQueryForParam;
+    return "./?" + searchQueryForParam + "&" + pageQueryForParam;
   };
+
+  const arrowsLeftClassOff = (n: number) => n <= 1;
+  const arrowsRightClassOff = (n: number) => n >= pagination - 1;
 
   return (
     <>
       <nav className="pages">
+        <div className="pages_arrows">
+          <Link
+            to={query(1)}
+            hidden={arrowsLeftClassOff(pageQuery > 3 ? 2 : 1)}
+          >
+            <button>
+              <i className="icon_chevrons-left"></i>
+            </button>
+          </Link>
+          <Link to={query(page - 1)} hidden={arrowsLeftClassOff(page)}>
+            <button>
+              <i className="icon_chevron-left"></i>
+            </button>
+          </Link>
+        </div>
         <div className="pages_n">
           {pagingMap
             .slice(pagingOffset - 3, pagingOffset + 2)
             .map((p: number) => {
               let selected = "";
-              if (p === queries.pageQuery) selected = "btn_selected";
+              if (p === page) selected = "btn_selected";
               return (
                 <Link to={query(p)} key={p}>
                   <button className={selected}>{p}</button>
@@ -50,12 +73,12 @@ export function Pages() {
             })}
         </div>
         <div className="pages_arrows">
-          <Link to={query(pageQuery + 1)}>
+          <Link to={query(page + 1)} hidden={arrowsRightClassOff(page)}>
             <button>
               <i className="icon_chevron-right"></i>
             </button>
           </Link>
-          <Link to={query(pagingMap.length)}>
+          <Link to={query(pagination - 1)} hidden={arrowsRightClassOff(page + 1)}>
             <button>
               <i className="icon_chevrons-right"></i>
             </button>
--- src/routes.tsx
+++ src/routes.tsx
@@ -1,14 +1,21 @@
-import { BrowserRouter, Switch, Route } from "react-router-dom";
+import { BrowserRouter, Switch, Route, useLocation } from "react-router-dom";
 
 import { Home } from "./pages/Home";
 
 const Routes = () => {
+  const basepath = () => {
+    // Condition for my site
+    let basepath = "/";
+    if (document.location.hostname === "savilexperiments.com.br") return "/marvel-chars_finder";
+    return basepath;
+  };
+
   return (
-      <BrowserRouter forceRefresh={true} >
-        <Switch>
-          <Route path="/" exact component={Home} />
-        </Switch>
-      </BrowserRouter>
+    <BrowserRouter forceRefresh={true}>
+      <Switch>
+        <Route path={basepath()} component={Home} />
+      </Switch>
+    </BrowserRouter>
   );
 };
 
